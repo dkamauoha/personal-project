33ff5326866b2adda17883c8a08e6716b2445393
Dylan Kamauoha <dylantkamauoha@gmail.com>
2018-07-31 12:23:44 -0600

    Routes Set Up

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
 
+//Routes
+import routes from './routes'
+
 //Components
-import About from './components/About/About';
-import Appointments from './components/Appointments/Appointments';
-import Gallery from './components/Gallery/Gallery';
-import Home from './components/Home/Home';
 import Nav from './components/Nav/Nav';
-import Services from './components/Services/Services';
-import Schedule from './components/Schedule/Schedule';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <About />
-        <Appointments />
-        <Gallery />
-        <Home />
         <Nav />
-        <Services />
-        <Schedule />
+        {routes}
+        
       </div>
     );
   }
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Nav () {
     return (
-        <h2>Nav</h2>
+        <div>
+            <h2>Nav</h2>
+            <div>
+                <Link to='/'><button>HOME</button></Link>
+                <Link to='/services'><button>SERVICES</button></Link>
+                <Link to='/gallery'><button>GALLERY</button></Link>
+                <Link to='/about'><button>ABOUT</button></Link>
+                <Link to='/schedule'><button>SCHEDULE AN APPOINTMENT</button></Link>
+                <Link to='/appointments'><button>APPOINTMENTS</button></Link>
+            </div>
+        </div>
     )
 }
 
